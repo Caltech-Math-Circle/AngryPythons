412d3a2a297bd4fd8ba15697c2f148bb5b4e8fef
chessstetson <chess@helynx.com>
2023-10-10 14:26:51 -0700

    trying to make the circle look more like a snake

--- pyangrybirds.py
+++ pyangrybirds.py
@@ -8,11 +8,10 @@ pygame.init()
 # Set up display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Parabolic Trajectories Game")
+pygame.display.set_caption("Snake-like Projectile Motion")
 
 # Colors
 WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
 # Define a function to calculate a parabolic trajectory
@@ -22,28 +21,12 @@ def parabolic_trajectory(start_pos, initial_velocity, angle, time):
     y = start_pos[1] - (initial_velocity * math.sin(math.radians(angle)) * time - 0.5 * g * time**2)
     return int(x), int(y)
 
-
-# Define a function to calculate the tangent at a point on the trajectory
-def tangent_at_point(start_pos, initial_velocity, angle, time):
-    g = 9.81
-    vx = initial_velocity * math.cos(math.radians(angle))
-    vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
-    return math.atan2(vy, vx)
-
-
-# Define a function to calculate the first derivative (tangent) at a point on the trajectory
-def first_derivative(start_pos, initial_velocity, angle, time):
-    g = 9.81
-    vx = initial_velocity * math.cos(math.radians(angle))
-    vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
-    return vx, vy
-
 # Define a function to calculate the slope of the tangent at a point on the trajectory
 def tangent_slope(start_pos, initial_velocity, angle, time):
     g = 9.81
     vx = initial_velocity * math.cos(math.radians(angle))
     vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
-    return vy / vx    
+    return vy / vx
 
 # Main game loop
 running = True
@@ -60,32 +43,25 @@ while running:
     # Draw the ground
     pygame.draw.rect(screen, GREEN, (0, height - 10, width, 10))
 
-    # Draw trajectory and first derivative at current time step
-    if t <= 100:
+    # Draw snake-like projectile at current time step
+    if t <= 200:
         start_pos = (100, height - 20)
-        initial_velocity = 100
+        initial_velocity = 40
         angle = 45
 
         time = t / 10
         pos = parabolic_trajectory(start_pos, initial_velocity, angle, time)
-        pygame.draw.circle(screen, BLUE, pos, 5)
+        pygame.draw.line(screen, GREEN, start_pos, pos, 5)
 
-        derivative = first_derivative(start_pos, initial_velocity, angle, time)
-        end_x = pos[0] + 20 * derivative[0]
-        end_y = pos[1] - 20 * derivative[1]
-        pygame.draw.line(screen, BLUE, pos, (end_x, end_y))
-
-
-        # slope = tangent_slope(start_pos, initial_velocity, angle, time)
-        # x1 = pos[0] - 20
-        # y1 = pos[1] - 20 * slope
-        # x2 = pos[0] + 20
-        # y2 = pos[1] + 20 * slope
-        # pygame.draw.line(screen, BLUE, (x1, y1), (x2, y2))
+        slope = tangent_slope(start_pos, initial_velocity, angle, time)
+        x1 = pos[0] - 20
+        y1 = pos[1] - 20 * slope
+        x2 = pos[0] + 20
+        y2 = pos[1] + 20 * slope
+        pygame.draw.line(screen, GREEN, (x1, y1), (x2, y2), 5)
 
         t += time_step
         pygame.display.flip()
     else:
         pygame.quit()
         sys.exit()
-

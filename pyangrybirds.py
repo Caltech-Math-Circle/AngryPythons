4716d5ff36ebc159823c7c951374eeb68681fcde
chessstetson <chess@helynx.com>
2023-10-10 12:34:51 -0700

    trying to get it to draw first derivative
    Can you redo this so that it draws the first derivative at each point?

--- pyangrybirds.py
+++ pyangrybirds.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import sys
 
 # Initialize Pygame
 pygame.init()
@@ -21,6 +22,7 @@ def parabolic_trajectory(start_pos, initial_velocity, angle, time):
     y = start_pos[1] - (initial_velocity * math.sin(math.radians(angle)) * time - 0.5 * g * time**2)
     return int(x), int(y)
 
+
 # Define a function to calculate the tangent at a point on the trajectory
 def tangent_at_point(start_pos, initial_velocity, angle, time):
     g = 9.81
@@ -28,8 +30,26 @@ def tangent_at_point(start_pos, initial_velocity, angle, time):
     vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
     return math.atan2(vy, vx)
 
+
+# Define a function to calculate the first derivative (tangent) at a point on the trajectory
+def first_derivative(start_pos, initial_velocity, angle, time):
+    g = 9.81
+    vx = initial_velocity * math.cos(math.radians(angle))
+    vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
+    return vx, vy
+
+# Define a function to calculate the slope of the tangent at a point on the trajectory
+def tangent_slope(start_pos, initial_velocity, angle, time):
+    g = 9.81
+    vx = initial_velocity * math.cos(math.radians(angle))
+    vy = -initial_velocity * math.sin(math.radians(angle)) - g * time
+    return vy / vx    
+
 # Main game loop
 running = True
+t = 0
+time_step = 0.1  # Time step for drawing the trajectory
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,21 +60,32 @@ while running:
     # Draw the ground
     pygame.draw.rect(screen, GREEN, (0, height - 10, width, 10))
 
-    # Draw trajectory
-    start_pos = (100, height - 20)
-    initial_velocity = 40
-    angle = 45
+    # Draw trajectory and first derivative at current time step
+    if t <= 100:
+        start_pos = (100, height - 20)
+        initial_velocity = 100
+        angle = 45
 
-    for t in range(20):
         time = t / 10
         pos = parabolic_trajectory(start_pos, initial_velocity, angle, time)
         pygame.draw.circle(screen, BLUE, pos, 5)
 
-        tangent_angle = tangent_at_point(start_pos, initial_velocity, angle, time)
-        end_x = pos[0] + 20 * math.cos(tangent_angle)
-        end_y = pos[1] - 20 * math.sin(tangent_angle)
+        derivative = first_derivative(start_pos, initial_velocity, angle, time)
+        end_x = pos[0] + 20 * derivative[0]
+        end_y = pos[1] - 20 * derivative[1]
         pygame.draw.line(screen, BLUE, pos, (end_x, end_y))
 
-    pygame.display.flip()
 
-pygame.quit()
+        # slope = tangent_slope(start_pos, initial_velocity, angle, time)
+        # x1 = pos[0] - 20
+        # y1 = pos[1] - 20 * slope
+        # x2 = pos[0] + 20
+        # y2 = pos[1] + 20 * slope
+        # pygame.draw.line(screen, BLUE, (x1, y1), (x2, y2))
+
+        t += time_step
+        pygame.display.flip()
+    else:
+        pygame.quit()
+        sys.exit()
+
